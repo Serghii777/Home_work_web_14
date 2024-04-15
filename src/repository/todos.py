841bbf1060e6bf22017434a61712d2bff2c0808e
Serghii777 <142201494+Serghii777@users.noreply.github.com>
2024-04-15 19:46:40 +0200

    second_commit

--- src/repository/todos.py
+++ src/repository/todos.py
@@ -6,18 +6,43 @@ from src.schemas.todo import ContactSchema, ContactUpdateSchema
 
 
 async def get_contacts(limit: int, offset: int, db: AsyncSession):
+    """
+    The get_contacts function returns a list of contacts from the database.
+    
+    :param limit: int: Set the maximum number of contacts to return
+    :param offset: int: Specify how many contacts to skip
+    :param db: AsyncSession: Pass the database session to the function
+    :return: A list of contact objects
+    :doc-author: Trelent
+    """
     stmt = select(Contact).offset(offset).limit(limit)
     contacts = await db.execute(stmt)
     return contacts.scalars().all()
 
 
 async def get_contact(contact_id: int, db: AsyncSession):
+    """
+    The get_contact function returns a contact object from the database.
+    
+    :param contact_id: int: Specify the contact id to be retrieved
+    :param db: AsyncSession: Pass the database connection to the function
+    :return: A single contact or none if the contact is not found
+    :doc-author: Trelent
+    """
     stmt = select(Contact).filter_by(id=contact_id)
     contact = await db.execute(stmt)
     return contact.scalar_one_or_none()
 
 
 async def create_contact(body: ContactSchema, db: AsyncSession):
+    """
+    The create_contact function creates a new contact in the database.
+    
+    :param body: ContactSchema: Validate the request body
+    :param db: AsyncSession: Get a database session
+    :return: The contact object that was created
+    :doc-author: Trelent
+    """
     contact = Contact(**body.model_dump(exclude_unset=True))
     db.add(contact)
     await db.commit()
@@ -26,6 +51,15 @@ async def create_contact(body: ContactSchema, db: AsyncSession):
 
 
 async def update_contact(contact_id: int, body: ContactUpdateSchema, db: AsyncSession):
+    """
+    The update_contact function updates a contact in the database.
+    
+    :param contact_id: int: Identify the contact to be updated
+    :param body: ContactUpdateSchema: Get the data from the request body
+    :param db: AsyncSession: Pass the database session to the function
+    :return: A contact object
+    :doc-author: Trelent
+    """
     stmt = select(Contact).filter_by(id=contact_id)
     result = await db.execute(stmt)
     contact = result.scalar_one_or_none()
@@ -42,6 +76,14 @@ async def update_contact(contact_id: int, body: ContactUpdateSchema, db: AsyncSe
 
 
 async def delete_contact(contact_id: int, db: AsyncSession):
+    """
+    The delete_contact function deletes a contact from the database.
+    
+    :param contact_id: int: Specify the id of the contact to be deleted
+    :param db: AsyncSession: Pass the database session to the function
+    :return: The contact that was deleted
+    :doc-author: Trelent
+    """
     stmt = select(Contact).filter_by(id=contact_id)
     contact = await db.execute(stmt)
     contact = contact.scalar_one_or_none()
--- src/repository/users.py
+++ src/repository/users.py
@@ -9,6 +9,15 @@ from src.schemas.user import UserSchema
 
 
 async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)):
+    """
+    The get_user_by_email function takes an email address and returns the user object associated with that email.
+    If no such user exists, it returns None.
+    
+    :param email: str: Pass in the email of the user we want to retrieve
+    :param db: AsyncSession: Pass the database session into the function
+    :return: A user object if the email exists in the database
+    :doc-author: Trelent
+    """
     stmt = select(User).filter_by(email=email)
     user = await db.execute(stmt)
     user = user.scalar_one_or_none()
@@ -16,6 +25,14 @@ async def get_user_by_email(email: str, db: AsyncSession = Depends(get_db)):
 
 
 async def create_user(body: UserSchema, db: AsyncSession = Depends(get_db)):
+    """
+    The create_user function creates a new user in the database.
+    
+    :param body: UserSchema: Validate the request body
+    :param db: AsyncSession: Get the database session
+    :return: The newly created user
+    :doc-author: Trelent
+    """
     avatar = None
     try:
         g = Gravatar(body.email)
@@ -31,17 +48,45 @@ async def create_user(body: UserSchema, db: AsyncSession = Depends(get_db)):
 
 
 async def update_token(user: User, token: str | None, db: AsyncSession):
+    """
+    The update_token function updates the user's refresh token in the database.
+    
+    :param user: User: Identify the user that is being updated
+    :param token: str | None: Specify the type of the token parameter
+    :param db: AsyncSession: Pass the database session to the function
+    :return: The user object
+    :doc-author: Trelent
+    """
     user.refresh_token = token
     await db.commit()
 
 
 async def confirmed_email(email: str, db: AsyncSession) -> None:
+    """
+    The confirmed_email function takes an email address and a database connection as arguments.
+    It then uses the get_user_by_email function to retrieve the user object associated with that email address.
+    The confirmed attribute of that user is set to True, and then the database is committed.
+    
+    :param email: str: Get the email of the user
+    :param db: AsyncSession: Pass in the database connection
+    :return: None
+    :doc-author: Trelent
+    """
     user = await get_user_by_email(email, db)
     user.confirmed = True
     await db.commit()
 
 
 async def update_avatar_url(email: str, url: str | None, db: AsyncSession) -> User:
+    """
+    The update_avatar_url function updates the avatar url for a user.
+    
+    :param email: str: Specify the email of the user whose avatar url is to be updated
+    :param url: str | None: Specify that the url parameter can be either a string or none
+    :param db: AsyncSession: Pass in a database session
+    :return: The updated user
+    :doc-author: Trelent
+    """
     user = await get_user_by_email(email, db)
     user.avatar = url
     await db.commit()
